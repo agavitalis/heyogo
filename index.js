4303fa3485615ff89bce9ea540ccb308f0869aad
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-02-20 00:59:31 +0100

    application setup complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors  from 'cors';
@@ -10,11 +11,21 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
+
 //db connection
 db_connection.connection_db()
 
+//static files
+app.use(express.static('public'))
+
 //Routes imported here
 app.use(web_routes);
+
+//view engine here
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 //Now tisten to this port 
 if (app.listen(process.env.PORT)) {
